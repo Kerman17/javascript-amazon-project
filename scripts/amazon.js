65160e92c43a2217f1e93c4731af6339b95ab9e8
Raul Detesan <deteraul@yahoo.com>
2025-04-07 12:47:51 +0300

    Use modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,9 @@
+import { cart } from '../data/cart.js';
+
 let productsHTML = '';
 
 products.forEach((product) => {
-    productsHTML += `
+  productsHTML += `
         <div class="product-container">
         <div class="product-image-container">
           <img class="product-image" src="${product.image}">
@@ -55,38 +57,38 @@ products.forEach((product) => {
 console.log(productsHTML);
 
 document.querySelector('.js-products-grid')
-    .innerHTML = productsHTML;
+  .innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart')
-    .forEach((button) => {
-        button.addEventListener('click', () => {
-            const productId = button.dataset.productId;
-            let matchingItem;
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      let matchingItem;
 
-            cart.forEach((item) => {
-                if (productId === item.productId) {
-                    matchingItem = item;
-                }
-            })
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      })
 
-            if (matchingItem) {
-                matchingItem.quantity += 1;
-            } else {
-                cart.push({
-                    productId,
-                    quantity: 1
-                });
-            }
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId,
+          quantity: 1
+        });
+      }
 
-            let cartQuantity = 0;
+      let cartQuantity = 0;
 
-            cart.forEach((item) => {
-                cartQuantity += item.quantity;
-            })
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
 
-            document.querySelector('.js-cart-quantity')
-                .innerHTML = cartQuantity;
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
 
 
-        })
-    });
+    })
+  });
